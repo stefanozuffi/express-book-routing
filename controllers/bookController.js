9565df402a7dab068bc0237cc4c5f679565f8ffc
stefanozuffi <stefano.zuffi06@outlook.com>
2025-09-16 15:22:21 +0200

    funzioni index e store definite

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,11 +1,29 @@
 const {books} = require('../data/bookArray.js')
 
 function index(req, res) {
-
+    let targets = books
+    if (req.query) {
+        
+        targets = books.filter(book => {
+            Object.keys(req.query).every(key => req.query[key] === book[key])
+        })
+    }
+   
+    res.json(targets)
 }
 
 function show(req, res) {
-    
+    const {id} = req.params.id
+    const book = books.find(b => b.id === parseInt(id))
+
+    if (!book) {
+        res.status(404).json({
+            error: true,
+            message: 'No correspondent id was found'
+        })
+    }
+
+    res.json(book)
 }
 
 function store(req, res) {
@@ -24,3 +42,11 @@ function destroy(req, res) {
     
 }
 
+module.exports = {
+    index,
+    show,
+    store,
+    update,
+    modify,
+    destroy
+}
--- data/bookArray.js
+++ data/bookArray.js
@@ -3,37 +3,37 @@ const books = [
         id: 1,
         title: "Il Signore degli Anelli",
         author: "J.R.R. Tolkien",
-        genre: "Fantasy"
+        genre: ["Fantasy", "Azione"]
     },
     {
         id: 2,
         title: "1984",
         author: "George Orwell",
-        genre: "Distopia"
+        genre: ["Distopia", "Sorveglianza"]
     },
     {
         id: 3,
         title: "Il Nome della Rosa",
         author: "Umberto Eco",
-        genre: "Storico"
+        genre: ["Storico", "Investigativo"]
     },
     {
         id: 4,
         title: "Cento Anni di Solitudine",
         author: "Gabriel García Márquez",
-        genre: "Realismo Magico"
+        genre: ["Realismo Magico"]
     },
     {
         id: 5,
         title: "Il Piccolo Principe",
         author: "Antoine de Saint-Exupéry",
-        genre: "Fiaba"
+        genre: ["Fiaba"]
     },
     {
         id: 6,
         title: "Orgoglio e Pregiudizio",
         author: "Jane Austen",
-        genre: "Romantico"
+        genre: ["Romantico"]
     }
 ];
 
